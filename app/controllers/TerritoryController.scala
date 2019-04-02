7c4875bf7dc86d3d72ed11243148221a4286cb50
Kyle <kwaldner3@gatech.edu>
2019-04-01 21:33:50 -0400

    fixed error messages in territory controller

--- app/controllers/TerritoryController.scala
+++ app/controllers/TerritoryController.scala
@@ -29,6 +29,7 @@ class TerritoryController @Inject()(cc: MessagesControllerComponents) extends Me
   private def isValidNum(str: String) = str != "" && isAllDigits(str)
   private def isInRange(str: String) = isValidNum(str) && str.toInt <= 47 && str.toInt >= 0
   private def territoryIsOccupied(terrIndex: Int): Boolean = GameData.terrArray(terrIndex).ownerName != ""
+  private def isArmyAmountInvalid(numArmies: Int): Boolean = numArmies <= 0 || numArmies > GameData.players(GameData.currPlayerIndex).armyBinCount
   private def getRandomIndex = {
     var randomter = scala.util.Random.nextInt(GameData.numTerritories)
     var iterations = 0
@@ -75,28 +76,29 @@ class TerritoryController @Inject()(cc: MessagesControllerComponents) extends Me
 
     val successFunction = { data: TerritoryData =>
       // This is the good case, where the form was successfully parsed as a Data object.
-      var terrIndex = -1
+      var terrIndex = -2
       if (startStateIncomplete) {
         Redirect(routes.TerritoryController.listTerritories()).flashing("Huh" -> "Something went wrong.")
       } else if (data.terr.toLowerCase() == "all random") {
         fillAll
-        terrIndex = -2
-        GameData.turnCounter = GameData.turnCounter - 1
+        terrIndex = -1
+        //GameData.turnCounter = GameData.turnCounter - 1
+        GameData.turnCounter = 0
       } else if (data.terr.toLowerCase() == "random") {
         terrIndex = getRandomIndex
       } else if (isInRange(data.terr) && !territoryIsOccupied(data.terr.toInt)) {
         terrIndex = data.terr.toInt
       }
-      if (terrIndex != -1) {
+      if (terrIndex > -2) {
         //success
-        if (terrIndex != -2) {
+        if (terrIndex != -1) {
           GameData.terrArray(terrIndex).incrementArmy(1)
           GameData.terrArray(terrIndex).setOwner(GameData.players(GameData.currPlayerIndex).name)
           GameData.players(GameData.currPlayerIndex).decrementArmyCount(1)
           newTurn
         }
         //now check turncounter
-        if (GameData.turnCounter != GameData.terrArray.length && terrIndex != -2) {
+        if (GameData.turnCounter != GameData.terrArray.length && terrIndex != -1) {
           Ok(views.html.armyview(GameData.players, GameData.currPlayerIndex, GameData.terrArray, terriform))
           val result = "Territory " + terrIndex + " now has " + GameData.terrArray(terrIndex).armyCount + " armies."
           Redirect(routes.TerritoryController.listTerritories()).flashing("Tubular! " -> result)
@@ -131,31 +133,29 @@ class TerritoryController @Inject()(cc: MessagesControllerComponents) extends Me
       var terrIndex = -1
       if (startStateIncomplete) {
         Redirect(routes.TerritoryController.updatePlacements).flashing("Huh" -> "Something went wrong.")
-      } else if (isInRange(data.terr)) {
-        if (GameData.terrArray(data.terr.toInt).ownerName == GameData.players(GameData.currPlayerIndex).name) {
-          terrIndex = data.terr.toInt
-        } else {
-          Redirect(routes.TerritoryController.updatePlacements).flashing("You can't do that! " -> "You don't own that territory.")
-        }
-      } else {
+      } else if (!isInRange(data.terr)) {
         Redirect(routes.TerritoryController.updatePlacements).flashing("Warning: " -> "This is not a valid territory value.")
-      }
-      if (data.numArmies <= 0 || data.numArmies > GameData.players(GameData.currPlayerIndex).armyBinCount) {
-          Redirect(routes.TerritoryController.updatePlacements).flashing("You and what army? " -> "That's more armies than you have.")
+      } else if (isInRange(data.terr) & !GameData.doesCurrPlayerOwnTerr(GameData.terrArray(data.terr.toInt))) {
+        Redirect(routes.TerritoryController.updatePlacements).flashing("You can't do that! " -> "You don't own that territory.")
       } else {
-          if (terrIndex != -1) {
-            //success
-            GameData.terrArray(terrIndex).incrementArmy(data.numArmies)
-            GameData.players(GameData.currPlayerIndex).decrementArmyCount(data.numArmies)
-            armiesOnTurn = armiesOnTurn + data.numArmies
-            if (GameData.players(GameData.currPlayerIndex).armyBinCount == 0) {
-              newTurn
-              assignNewArmies
-            }
+        terrIndex = data.terr.toInt
+        if(isArmyAmountInvalid(data.numArmies.toInt)) {
+          Redirect(routes.TerritoryController.updatePlacements).flashing("You and what army? " -> "That's more armies than you have.")
+        } else {
+          //success
+          GameData.terrArray(terrIndex).incrementArmy(data.numArmies)
+          GameData.players(GameData.currPlayerIndex).decrementArmyCount(data.numArmies)
+          armiesOnTurn = armiesOnTurn + data.numArmies
+          if (GameData.players(GameData.currPlayerIndex).armyBinCount == 0) {
+            newTurn
+            assignNewArmies
           }
           Ok(views.html.armyPlacement(GameData.players, GameData.currPlayerIndex, GameData.terrArray, additionalArmiesForm))
+
+        }
       }
     }
+
     val formValidationResult = additionalArmiesForm.bindFromRequest
     formValidationResult.fold(errorFunction, successFunction)
   }
