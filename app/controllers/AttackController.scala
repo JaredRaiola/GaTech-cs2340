d1abfeaa02c30c77a0d87189f570f112bc89c9ee
seong <sryoo6@gatech.edu>
2019-04-10 18:35:10 -0400

    Fix

--- app/controllers/AttackController.scala
+++ app/controllers/AttackController.scala
@@ -116,12 +116,23 @@ class AttackController @Inject()(cc: MessagesControllerComponents) extends Messa
       }
 
       // Stubbed case
-      GameData.newTurn
+      newTurn
+      assignNewArmies
       Redirect(routes.TerritoryController.updatePlacements()).flashing("Whoa! " -> "We haven't coded this functionality yet!")
 
     }
 
     val formValidationResult = attackForm.bindFromRequest
     formValidationResult.fold(errorFunction, successFunction)
+  }
+
+  private def newTurn = {
+    GameData.newTurn
+  }
+
+  private def assignNewArmies = {
+    var index = GameData.currPlayerIndex
+    var newArmies = GameData.calculateNewArmies(index)
+    GameData.players(index).incrementArmyCount(newArmies)
   }
 }
