6bf587a5e59e2100a19f5e16e3b5697dc1d32d42
seong <sryoo6@gatech.edu>
2019-04-12 14:35:47 -0400

    Apply final game loop fix

--- app/controllers/AttackForm.scala
+++ app/controllers/AttackForm.scala
@@ -4,12 +4,12 @@ object AttackForm {
   import play.api.data.Form
   import play.api.data.Forms._
 
-  case class AttackData(terr: Int, otherTerr: Int, attackDiceCount: Int, defenceDiceCount: Int)
+  case class AttackData(terr: String, otherTerr: String, attackDiceCount: Int, defenceDiceCount: Int)
 
   val attackForm = Form(
     mapping(
-      "terr" -> number,
-      "otherTerr" -> number,
+      "terr" -> nonEmptyText,
+      "otherTerr" -> nonEmptyText,
       "attackDiceCount" -> number,
       "defenceDiceCount" -> number
     )(AttackData.apply)(AttackData.unapply)
