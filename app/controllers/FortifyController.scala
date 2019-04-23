32f3afad8d14f65b5c2b5f3761e1014a8e0278e5
Mustafa Hussain <mustafa.hussain2357@gmail.com>
2019-04-23 16:03:45 -0400

    working fortify controller without logic

--- app/controllers/FortifyController.scala
+++ app/controllers/FortifyController.scala
@@ -13,21 +13,16 @@ class FortifyController @Inject()(cc: MessagesControllerComponents) extends Mess
   import FortifyForm._
 
   def updateFortifyView:Action[AnyContent] = Action { implicit request: MessagesRequest[AnyContent] =>
-    // Pass an unpopulated form to the template
     Ok(views.html.fortifyView(fortifyForm))
   }
 
-  // This will be the action that handles our form post
   def fortifyTerritory:Action[AnyContent] = Action { implicit request: MessagesRequest[AnyContent] =>
     val errorFunction = { formWithErrors: Form[FortifyData] =>
-      // This is the bad case, where the form had validation errors.
-      // Let's show the user the form again, with the errors highlighted.
-      // Note how we pass the form with errors to the template.
       BadRequest(views.html.fortifyView(formWithErrors))
     }
 
     val successFunction = { data: FortifyData =>
-      // This is the good case, where the form was successfully parsed as a Data object.
+
       Redirect(routes.FortifyController.updateFortifyView).flashing("This" -> " Works")
     }
     val formValidationResult = fortifyForm.bindFromRequest
